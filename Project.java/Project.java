1493a5281b3ded3c397c9c06fc963a8dd7205354
THRINATH-REDDY <thrinathreddy.lella@gmail.com>
2026-09-12 14:35:40 +0530

    added by thrinath

--- Project.java/Project.java
+++ Project.java/Project.java
@@ -2,19 +2,9 @@ import java.util.Scanner;
 public class Project {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-<<<<<<< HEAD
-        System.out.println("hii");
-        System.out.println("ganesh");
-
-
-
-
-
-=======
         System.out.println("Hello world");
         System.out.println("we done it trinath we will work now ");
         System.out.println("thrinath");
->>>>>>> 544471a45a8b2a48097feadf8ceaaa98d237fd08
 
 
 
