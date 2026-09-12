db372a4ffd446ae373f7851a235c24d9be8dcfea
medaramganesh <medaramsuneethasuneetha@gmail.com>
2026-09-12 15:15:57 +0530

    starting of project

--- Project.java/Project.java
+++ Project.java/Project.java
@@ -1,13 +1,8 @@
+//university grade and attendence trackor
 import java.util.Scanner;
 public class Project {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Hello world");
-        System.out.println("we done it trinath we will work now ");
-        System.out.println("thrinath");
-        System.out.println("hello");
-
-
-
+      
     }
 }
