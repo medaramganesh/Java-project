b10e176aa6833eba271f5d7c5084496df14c7deb
medaramganesh <medaramsuneethasuneetha@gmail.com>
2026-09-12 10:08:31 +0530

    new code

--- Project.java/Project.java
+++ Project.java/Project.java
@@ -1,14 +1,5 @@
-import java.util.Scanner;
 public class Project {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-     
-
-
-
-
-
-
-
+        System.out.println("Hello world");
     }
 }
