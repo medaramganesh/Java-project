b1a8dd7932be2a9529805aed12a05376e9ac38e7
medaramganesh <medaramsuneethasuneetha@gmail.com>
2026-09-12 10:46:17 +0530

    new code

--- Project.java/Project.java
+++ Project.java/Project.java
@@ -3,5 +3,6 @@ public class Project {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Hello world");
+        System.out.println("we done it trinath we will work now ");
     }
 }
