b7e1f5b63858ca0d761f75f3d281c1a2f8d32b24
THRINATH-REDDY <thrinathreddy.lella@gmail.com>
2026-09-12 14:39:10 +0530

    thrinath added

--- Project.java/Project.java
+++ Project.java/Project.java
@@ -5,6 +5,7 @@ public class Project {
         System.out.println("Hello world");
         System.out.println("we done it trinath we will work now ");
         System.out.println("thrinath");
+        System.out.println("hello");
 
 
 
